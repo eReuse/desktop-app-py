9fd750cb004331f90280b1bfdac27fff6bb58775
JNadeu <jnadeu@gmail.com>
2018-04-05 12:44:39 +0200

    workbench function

--- desktop_app/flaskapp.py
+++ desktop_app/flaskapp.py
@@ -2,13 +2,15 @@ from ereuse_utils import ensure_utf8
 from ereuse_workbench.workbench import Workbench
 from flask import Flask, render_template, jsonify
 from werkzeug.exceptions import NotFound
-import sched
+"""import sched"""
+
 
 class DesktopApp(Flask):
-    """ todo afterInstall.sh Systemd root service desktop-app
+    """ todo Systemd root service desktop-app
         todo python scheduler per correr periodicament
         todo create icon.desktop
     """
+
     def __init__(self, import_name=__name__, static_path=None, static_url_path=None,
                  static_folder='static', template_folder='templates', instance_path=None,
                  instance_relative_config=False, root_path=None):
@@ -18,17 +20,17 @@ class DesktopApp(Flask):
         self.workbench = Workbench()
         self.add_url_rule('/', view_func=self.view_default, methods={'GET'})
         self.add_url_rule('/info', view_func=self.view_info, methods={'GET'})
-        self.add_url_rule('/workbench', view_func=self.view_workbench, methods={'POST'})
+        # self.add_url_rule('/workbench', view_func=self.view_workbench, methods={'POST'})
 
     def view_info(self):
         raise NotFound()
 
-    def view_workbench(self):
+    def execute_workbench(self):
         """Execute workbench manually and returns the result.
-            Need to be root user"""
+            Need to be root user
+        """
         snapshot = self.workbench.run()
         return jsonify(snapshot)
-
 
     def view_default(self):
         """Import template index.html"""
