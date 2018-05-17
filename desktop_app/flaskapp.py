82d938e9a366f771da2baad7a6a8bc91f9baaa52
JNadeu <jnadeu@gmail.com>
2018-05-17 13:12:07 +0200

    refactor lastSnapshot

--- desktop_app/flaskapp.py
+++ desktop_app/flaskapp.py
@@ -10,7 +10,6 @@ from ereuse_utils import ensure_utf8, DeviceHubJSONEncoder
 from ereuse_utils.test import Client
 from ereuse_workbench.workbench import Workbench
 from flask import Flask, render_template, jsonify, request, Response
-from werkzeug.exceptions import NotFound
 
 
 class DesktopApp(Flask):
@@ -22,18 +21,25 @@ class DesktopApp(Flask):
     test_client_class = Client
     json_encoder = DeviceHubJSONEncoder
 
-    def __init__(self, import_name=__name__, static_path=None, static_url_path=None,
-                 static_folder='static', template_folder='templates', instance_path=None,
-                 instance_relative_config=False, root_path=None):
+    def __init__(self,
+                 import_name=__name__,
+                 static_path=None,
+                 static_url_path=None,
+                 static_folder='static',
+                 template_folder='templates',
+                 instance_path=None,
+                 instance_relative_config=False,
+                 root_path=None,
+                 path_env=Path(__file__).parent.joinpath('.env_dh.json')
+                 ):
         ensure_utf8(self.__class__.__name__)
         super().__init__(import_name, static_path, static_url_path, static_folder, template_folder,
                          instance_path, instance_relative_config, root_path)
         self.add_url_rule('/', view_func=self.view_default, methods={'GET'})
-        self.add_url_rule('/info', view_func=self.view_info, methods={'GET'})
         self.add_url_rule('/workbench', view_func=self.view_workbench, methods={'GET', 'POST'})
-        # load env with config values
-        # noinspection PyUnresolvedReferences
-        with Path(__file__).parent.joinpath('.env_dh.json').open('r') as file:
+        # Open last env with config values
+        # noinspection PyUnresolvedReferences,
+        with path_env.open('r') as file:
             self.env_dh = json.load(file)  # type: dict
             assert isinstance(self.env_dh, dict)
 
@@ -46,7 +52,8 @@ class DesktopApp(Flask):
             self.new_config_response = requests.get(url=url_info, data=snapshot_init)
         else:
             self.new_config_response = requests.get(url=url_info)
-        self.env_dh = self.new_config_response.json()
+        # self.env_dh = self.new_config_response.json()
+        # Save new config in env file
         with Path(__file__).parent.joinpath('.env_dh.json').open('w') as file:
             json.dump(self.env_dh, file)
 
@@ -54,15 +61,12 @@ class DesktopApp(Flask):
         self.workbench_thread = WorkbenchThread(self.workbench_queue)
         self.workbench_thread.run()
 
-        last_snapshot = parse(self.env_dh['lastWorkbench']) # type: datetime
+        last_snapshot = parse(self.env_dh['lastSnapshot'])  # type: datetime
         days_between_snapshots = timedelta(days=self.env_dh['daysBetweenSnapshots'])
 
         # compare diff between dates and start wb process
         if (datetime.now() - last_snapshot) >= days_between_snapshots:
             self.workbench_queue.put(None)
-
-    def view_info(self):
-        raise NotFound()
 
     def view_workbench(self):
         """Execute workbench manually and returns snapshot to DH.
